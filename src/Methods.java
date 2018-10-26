dc9d4d1fe6e7c492b006932cec7757910dd717d3
Logistyk <kratiukkarol@gmail.com>
2018-10-26 16:04:59 +0200

    changed methods

--- src/Methods.java
+++ src/Methods.java
@@ -2,17 +2,14 @@ import java.util.Scanner;
 
 public class Methods {
 
-    private static int result;
-
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
         System.out.println("Type a number");
-        int result = in.nextInt();
-        Methods.result = result;
         isPositive();
     }
 
     private static boolean isPositive() {
+        Scanner in = new Scanner(System.in);
+        int result = in.nextInt();
         if(result>=0){
             System.out.println("Number is positive");
             return true;
@@ -20,5 +17,6 @@ public class Methods {
             System.out.println("Number is negative");
             return false;
         }
+
     }
 }
